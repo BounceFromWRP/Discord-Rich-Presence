9c73ec63faa920cb156609e80eb8867bcb8c96dd
realityislarry <122501824+realityislarry@users.noreply.github.com>
2024-06-08 01:55:22 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean' -- Currently compatible with my server version: 620820541, build: ~1600 approx.
+game 'gta5'
+
+author 'East Coast Developememt'
+description 'FiveM Discord Rich Presence by ECD'
+
+client_scripts {
+    "discord.lua", -- this will hold the data we are going to use
+}
